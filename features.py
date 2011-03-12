72d2434855ab162f12320221c2a78451ebcce4f4
Mason Chua <mason_chua@aftran.com>
2011-03-11 21:15:09 -0800

    Refactored, and added some, features.

--- features.py
+++ features.py
@@ -24,52 +24,52 @@ def words_in(field):
 		return map(mkfeature, wordset) # TODO: The lambda is a stub.
 	return result
 
-feature_templates = [
+feature_templates = map(make_searcher, [
 	# These seem likely to show up in articles about disease:
-	make_searcher('estimated'),
-	make_searcher('exposure'),
-	make_searcher('percent'),
-	make_searcher('population'),
-	make_searcher('virus'),
-	make_searcher('disease'),
-	make_searcher('outbreak'),
-	make_searcher('report'),
-	make_searcher('prevention'),
-	make_searcher('control'),
-	make_searcher('disease'),
-	make_searcher('confirmed'),
-	make_searcher('diagnosed'),
-	make_searcher('diagnosis'),
-	make_searcher('associated'),
-	make_searcher('emergency'),
-	make_searcher('(epi|pan)demic', 'title'),
-	make_searcher('season'), # TODO: Maybe exclude 'hurricane'.
-	make_searcher('dea(d|th)'),
-	# make_searcher('sick'), 	# Doesn't distinguish.
-	# make_searcher('\sill\s'), 	# Doesn't distinguish.
-	# make_searcher('issued.*advisory'), # Too sparse.
-	# Template:
-	# make_searcher(''),
+	'estimated',
+	'exposure',
+	'percent',
+	'population',
+	'virus',
+	'disease',
+	'outbreak',
+	'report',
+	'prevention',
+	'control',
+	'disease',
+	'confirmed',
+	'diagnosed',
+	'diagnosis',
+	'associated',
+	'emergency',
+	'season', # TODO: Maybe exclude 'hurricane'.
+	'dea(d|th)',
+	'authorities',
+	# 'sick', 		# Doesn't distinguish.
+	# '\sill\s', 		# Doesn't distinguish.
+	# 'issued.*advisory', 	# Too sparse.
 
 	# Things I'm trying out because of their counts:
-	make_searcher('president'),
-	make_searcher('&lt;img'), # Has a picture.  Good for 'unrelated'.
+	'president',
+	'&lt;img', # Has a picture.  Good for 'unrelated'.
 
 	# These seem like they would show up in non-outbreak or non-disease articles:
-	make_searcher('rival'),
-	make_searcher('sport'),
-	make_searcher('champ'),
-	make_searcher('protest'),
-	make_searcher('hurricane'),
-	make_searcher('economy'),
-	make_searcher('wedding'), # Good but sparse.
-	make_searcher('research'),
-	make_searcher('prosecut'),
-
+	'rival',
+	'sport',
+	'champ',
+	'protest',
+	'hurricane',
+	'economy',
+	'wedding', # Good but sparse.
+	'research',
+	'prosecut'] # as in 'prosecution' and 'prosecuted'.
+	) + [
 	# TODO: URL features.
-
-	words_in('title')
+	make_searcher('(epi|pan)demic', 'title'),
+	make_searcher('H\dN\d', field='article_snippet', flags=0), # names of flu subtypes, like H1N1
+	words_in('title'),
 	# TODO: Maybe strip away non-word symbols like in "(ap)" and "prosecutors:".
 	# TODO: Maybe get the word STEMS in the title, not the words.
+	# TODO: Replace all numbers with a single digit or something.
 
-]
+	]
