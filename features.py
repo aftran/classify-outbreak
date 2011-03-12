f1e5917250053be3b0f01ba54eeee1da5160ba86
Mason Chua <mason_chua@aftran.com>
2011-03-12 12:26:51 -0800

    Added named_entities feature and reorganized.

--- features.py
+++ features.py
@@ -1,11 +1,12 @@
 from re import search, IGNORECASE
+import nltk
 
-"Some baseline features for testing the classifier."
+"Features for the classifier."
 
 def make_searcher(pattern, field='article_snippet', flags=IGNORECASE):
 	"""Returns a feature template function that defines the feature
 	   'has_pattern_' + pattern, which is present whenever the datum
-	   contains that pattern.  Default by case-insensitive.
+	   contains that pattern.  Defaults to case-insensitive.
 	"""
 	def result(datum):
 		if search(pattern, datum.__dict__[field], flags):
@@ -21,10 +22,25 @@ def words_in(field):
 		def mkfeature(string): return field + '_has_word_' + string
 		for word in datum.__dict__[field].split():
 			wordset.add(word.lower())
-		return map(mkfeature, wordset) # TODO: The lambda is a stub.
+		return map(mkfeature, wordset)
 	return result
 
-feature_templates = map(make_searcher, [
+# Since NER is slow, only do it for the first cutoff_for_ner characters of the
+# article_snippet:
+cutoff_for_ner = 1000
+# Again, since NER is slow, only do it for the first
+# per_sentence_cutoff_for_ner characters of each sentence:
+per_sentence_cutoff_for_ner = 10
+
+def named_entities(datum):
+	result = []
+	for sent in nltk.sent_tokenize(datum.article_snippet[:cutoff_for_ner]):
+		for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent[:per_sentence_cutoff_for_ner]))):
+			if hasattr(chunk, 'node'):
+				result.append('article_snippet_has_entity_' + chunk.node + '_' +  '_'.join(c[0].lower() for c in chunk.leaves()))
+	return result
+
+matcher_features = map(make_searcher, [
 	# These seem likely to show up in articles about disease:
 	'estimated',
 	'exposure',
@@ -61,10 +77,14 @@ feature_templates = map(make_searcher, [
 	'hurricane',
 	'economy',
 	'wedding', # Good but sparse.
+	'linux',
 	'research',
 	'prosecut'] # as in 'prosecution' and 'prosecuted'.
-	) + [
+	)
+
+other_features = [
 	# TODO: URL features.
+	named_entities,
 	make_searcher('(epi|pan)demic', 'title'),
 	make_searcher('H\dN\d', field='article_snippet', flags=0), # names of flu subtypes, like H1N1
 	words_in('title'),
@@ -73,3 +93,5 @@ feature_templates = map(make_searcher, [
 	# TODO: Replace all numbers with a single digit or something.
 
 	]
+
+feature_templates = matcher_features + other_features
